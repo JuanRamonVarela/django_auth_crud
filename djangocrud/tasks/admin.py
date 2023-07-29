2da1898d28b2a32d8b4ecfba3ef9627f24c6fec8
Ramón Varela <55912727+JuanRamonVarela@users.noreply.github.com>
2023-07-28 21:45:22 -0500

    Delete djangocrud directory

--- djangocrud/tasks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import tasks
-# Register your models here.
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields=("created", "id")
-admin.site.register(tasks, TaskAdmin)
